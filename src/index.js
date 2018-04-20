8c38b75948c600bfaf31637c095c7b96fb778c46
yashoda4A3 <ssdvy.p@gmail.com>
2018-04-20 10:02:12 +0530

    User Authentication

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import express from 'express';
 import path from 'path';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+// Promise from 'bluebird';
+import auth from './routes/auth';
 
+dotenv.config();
 const app = express();
-app.post('/api/auth', (req, res) => {
-  res.status(400).json({ errors: { global: 'Invalid credentials' } });
-});
+app.use(bodyParser.json());
+// mongoose.Promise = Promise;
+mongoose.connect(process.env.MONGODB_URL);
+
+app.use('/api/auth', auth);
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
