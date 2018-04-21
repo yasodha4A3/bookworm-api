5d6a35baab3c93e6d4200ec2a6e6764991ed895e
yashoda4A3 <ssdvy.p@gmail.com>
2018-04-21 17:24:02 +0530

    user signs up

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
 // Promise from 'bluebird';
 import auth from './routes/auth';
+import users from './routes/users';
 
 dotenv.config();
 const app = express();
@@ -13,6 +14,7 @@ app.use(bodyParser.json());
 mongoose.connect(process.env.MONGODB_URL);
 
 app.use('/api/auth', auth);
+app.use('/api/users', users);
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
